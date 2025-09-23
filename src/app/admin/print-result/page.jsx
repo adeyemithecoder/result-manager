bc5c78a0e6b3f8baa3e974f788d7cb971fade538
adeyemithecoder <adeyemithecoder@gmail.com>
2025-09-23 16:51:37 +0100

    add old year

--- src/app/admin/print-result/page.jsx
+++ src/app/admin/print-result/page.jsx
@@ -117,6 +117,7 @@ const StudentResult = () => {
             <option value="" disabled>
               Select academic year
             </option>
+            <option value="2024/2025">2024/2025</option>
             <option value="2025/2026">2025/2026</option>
           </select>
 
